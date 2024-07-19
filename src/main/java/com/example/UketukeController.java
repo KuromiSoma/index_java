52d9d8a4be31dac3c396a6de7b7f4199eff6c662
KuromiSoma <tamagoyaki.zyukuzyuku@gmail.com>
2024-07-19 18:17:49 +0900

    合ってるか不安

---
+++ src/main/java/com/example/UketukeController.java
@@ -0,0 +1,37 @@
+package com.example;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@Controller
+public class UketukeController {
+
+	@Autowired
+	private UketukeService uketukeService;
+
+	@RequestMapping(path = "/uketuke", method = RequestMethod.GET)
+	String index(Model model) {
+		List<Uketuke> list = this.uketukeService.getUketukeList();
+	    model.addAttribute("list", list);
+	    return "uketuke/index";
+	}
+	
+	@RequestMapping(path = "/member", method = RequestMethod.GET)
+	String member(Model model) {
+	    return "/uketuke/member";
+	}
+	
+	@RequestMapping(path = "/member", method = RequestMethod.POST)
+	String memberReg(Model model, @ModelAttribute  UketukeForm uketukeForm ) {
+		this.uketukeService.insertUketuke(model, uketukeForm);
+	    List<Uketuke> list = this.uketukeService.getUketukeList();
+	    model.addAttribute("list", list);
+		return "/uketuke/index";
+	}
+	
+}
