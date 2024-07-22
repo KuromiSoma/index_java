6b6bd88cdf04801ae928ac4eff3b176e2cfbd5a0
KuromiSoma <tamagoyaki.zyukuzyuku@gmail.com>
2024-07-22 13:01:32 +0900

    広田さんこれ？

---
+++ src/main/java/com/example/Test20230325Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test20230325Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test20230325Application.class, args);
+	}
+
+}
